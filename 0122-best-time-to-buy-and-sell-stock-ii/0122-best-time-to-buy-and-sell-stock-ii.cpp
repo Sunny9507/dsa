68fd2aa074dd098f4cc437d6a3764d28c1f0a905
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-06-15 10:26:38 +0530

    Time: 6 ms (49.35%), Space: 15.5 MB (70.33%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,46 +1,13 @@
-// class Solution {
-// public:
-//     int maxProfit(vector<int>& prices) {
-//         int mini= INT_MAX, maxi = 0;
-//         int profit1 = 0, profit2 = 0;
-//         for(int price : prices){
-//             mini = min(mini, price);
-//             maxi = max(maxi, price-mini);
-//             profit1 = max(profit1, maxi);
-//             profit2 = max(profit2, price - profit1)
-//         }
-
-//     }
-
-
-
-// class Solution {
-// public:
-//     int maxProfit(vector<int>& prices) {
-//         int profit = 0;
-//         for(int i = 1; i < prices.size(); i++) {
-//             if(prices[i] > prices[i-1]) {
-//                 profit += prices[i] - prices[i-1];
-//             }
-//         }
-//         return profit;
-//     }
-// };
-
-
-
-
-
-
 
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int hold = -INT_MAX, sold = 0;
-        for (int price : prices) {
-            hold = max(hold, sold - price);
-            sold = max(sold, hold + price);
+        int profit = 0;
+        for(int i = 1; i < prices.size(); i++) {
+            if(prices[i] > prices[i-1]) {
+                profit += prices[i] - prices[i-1];
+            }
         }
-        return max(hold, sold);
+        return profit;
     }
 };
