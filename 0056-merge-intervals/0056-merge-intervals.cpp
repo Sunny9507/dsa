319ede614c1fd945fcae1ca5b7862cc88045bf19
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-07-06 16:10:05 +0530

    Time: 28 ms (26.39%), Space: 23.7 MB (20.01%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,30 +3,19 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
 
-        int n = intervals.size();
-        vector<vector<int>> ans;
-
-        for(int i=0; i<n; i++){
-            if(ans.empty()){
-                ans.push_back(intervals[i]);
+        vector<vector<int>> merged;
+        for (auto interval : intervals) {
+            // if the list of merged intervals is empty or if the current
+            // interval does not overlap with the previous, simply append it.
+            if (merged.empty() || merged.back()[1] < interval[0]) {
+                merged.push_back(interval);
             }
-            else{
-                vector<int>&v = ans.back();
-                int y = v[1]; //end time of previous interval
-                //compare y with i's start time curret interval
-                
-                //[1, 4] [2, 5]
-                //[1, 6]
-                if(intervals[i][0] <= y){
-                    v[1] = max(y, intervals[i][1]);
-                }
-                // not overlapping
-                else{
-                    ans.push_back(intervals[i]);
-                }
-
+            // otherwise, there is overlap, so we merge the current and previous
+            // intervals.
+            else {
+                merged.back()[1] = max(merged.back()[1], interval[1]);
             }
         }
-        return ans;
+        return merged;
     }
 };
