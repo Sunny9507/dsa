683f9aa9c582cbafa3f2842cea9ca5d3dbbcaa59
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-07-06 20:47:24 +0530

    Time: 0 ms (100%), Space: 14.5 MB (54.48%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
-     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        int ind = -1;
-        for(int i = n-2; i>=0; i--){
-            if(nums[i]<nums[i+1]){
-                ind = i;
+    void nextPermutation(vector<int>& nums) {
+        if(nums.size() == 1){
+            return ;
+        }
+
+        int idx1;
+        for(int i=nums.size()-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                idx1 = i;
                 break;
             }
         }
 
-        if(ind == -1){
+        if(idx1 < 0){
             reverse(nums.begin(), nums.end());
-            return;
         }
-        
-        for(int i = n-1; i>ind; i--){
-            if(nums[i]>nums[ind]){
-                swap(nums[i], nums[ind]);
-                break;
-            }
-        }
+        else{
 
-        reverse(nums.begin()+ ind+1, nums.end());
-        
+            int idx2 = 0;
+            for(int i=nums.size()-1; i>=0; i--){
+                if(nums[i] > nums[idx1]){
+                    idx2 = i;
+                    break;
+                }
+            }
+            
+            swap(nums[idx1], nums[idx2]);
+
+            sort(nums.begin()+ idx1+1, nums.end());
+        }
     }
 };
