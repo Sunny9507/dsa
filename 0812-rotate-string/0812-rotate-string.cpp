928dc22f647c10fbc6410aeb4ae8ece3977b7bd7
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-07-21 00:00:52 +0530

    Time: 3 ms (47.6%), Space: 7.6 MB (38.93%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -3,15 +3,12 @@
 class Solution {
 public:
     bool rotateString(std::string s, std::string goal) {
-        // Check if the lengths are different
         if (s.length() != goal.length()) {
             return false;
         }
         
-        // Concatenate s with itself
-        std::string concatenated = s + s;
+        string concatenated = s + s;
         
-        // Check if goal is a substring of the concatenated string
-        return concatenated.find(goal) != std::string::npos;
+        return concatenated.find(goal) != string::npos;
     }
 };
