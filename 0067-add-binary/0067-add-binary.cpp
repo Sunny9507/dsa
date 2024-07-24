556068eff1607eb55ec5184d8ed706e17fe8a1af
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-07-24 21:14:24 +0530

    Time: 4 ms (44.09%), Space: 12.3 MB (5.91%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,53 +1,26 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-         if(b.size() > a.size()) swap(a,b);
-        
-        
-        while(b.size() < a.size()) b = "0" + b;
+         int alen = a.length();
+         int blen = b.length();
+         
+         int i=0, carry = 0;
+         string ans = "";
 
-        int carry = 0;
+         while(i < alen || i < blen || carry != 0){
+            int x = 0;
+            if(i < alen && a[alen-i-1] == '1'){
+                x = 1;
+            }
 
-        string res = "";
-
-        for(int i = b.size()-1; i >= 0 ; --i)
-        {
-             
-             if(b[i] == '1' && a[i]=='1')
-             {
-
-                if(carry == 0) res = "0" + res;
-                
-                else res = "1" + res;
-                    
-                carry = 1;
-             }
-
-             else if(b[i] =='0' && a[i] =='0')
-             {
-
-                if(carry == 0) res = "0" + res;
-                 
-                else
-                {
-                    res = "1" + res;
-                    carry = 0;
-                }
-             }
-
-             else if((b[i]=='0' && a[i]=='1') || (b[i]=='1' && a[i] == '0'))
-             {
-                 
-                if(carry == 0) res = "1" + res;
-                 
-                else res = "0" + res;
-                 
-             }
-             
-        }
-        
-        if(carry == 1) res = "1" + res;
-        
-        return res;
+            int y=0;
+            if(i < blen && b[blen-i-1] == '1'){
+                y = 1;
+            }
+            ans = to_string((x + y + carry) % 2) + ans;
+            carry = (x + y + carry) / 2;
+            i += 1;
+         }
+         return ans;
     }
 };
