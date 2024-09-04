b89ea25d2f4eee0bac006e49445d7a1f35c5b322
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-09-04 20:20:29 +0530

    Time: 0 ms (100%), Space: 13.7 MB (84.65%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,46 +1,32 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
 
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] == target) return i;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            
+            if (nums[left] <= nums[mid]) { 
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1; 
+                } else {
+                    left = mid + 1; 
+                }
+            } else { 
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1; 
+                } else {
+                    right = mid - 1; 
+                }
+            }
         }
+
         return -1;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-// public:
-//     int search(vector<int>& nums, int target) {
-//         int n = nums.size();
-//         int l=0, h = n-1;
-
-//         while(l <= h){
-//             int m = l+(h - l)/2;
-
-//             if(nums[m] == target) return m;
-//             else if(target < nums[m]){
-//                 h = m - 1;
-//             }
-//             else{
-//                 l = m + 1;
-//             }
-
-//         }
-//         return -1;
-//     }
-// };
