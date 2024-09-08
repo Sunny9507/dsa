9a83160cb5a1134c37bc00df7a36ab50f6bdbb17
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-09-09 00:55:24 +0530

    Time: 17 ms (28.12%), Space: 20.6 MB (76.41%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        int xor1 = 0, xor2 = 0;
         int n = nums.size();
-        int sum = n*(n+1)/2;
-        int s = 0;
-        for(int i=0; i<nums.size(); i++){
-            s += nums[i];
+        
+        // XOR all the numbers in the array
+        for (int i = 0; i < n; i++) {
+            xor2 ^= nums[i];
+            xor1 ^= (i+1);
         }
-        return sum - s;
+        
+        // XOR all the indices from 0 to n
+        // for (int i = 0; i <= n; i++) {
+        //     xor1 ^= i;
+        // }
+        
+        // The missing number is the XOR of both results
+        return xor1 ^ xor2;
     }
 };
