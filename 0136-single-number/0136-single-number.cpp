316d63042e66051e9201ce062d8d91f4801e22d4
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-09-09 01:12:20 +0530

    Time: 14 ms (59.95%), Space: 19.6 MB (69.53%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,18 +1,27 @@
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         int n = nums.size();
+
+//         for(int i=0; i<n; i++)
+//             for(int j=0; j<n; j++){
+                    
+//                 if(nums[i]) == nums[j])
+//                     cnt++;
+//             }
+//             if(cnt == 1) return nums[i]
+//     }
+//         return 0;
+//     }
+// };
+
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n = nums.size();
-
-        unordered_map<int, int>m;
-
-        for(int i=0; i<n; i++)
-            m[nums[i]]++;
-
-            for(int i=0; i<n; i++){
-                    
-                if(m.at(nums[i]) == 1)
-                    return nums[i];
-            }
-        return 0;
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;  
+        }
+        return result;  
     }
 };
