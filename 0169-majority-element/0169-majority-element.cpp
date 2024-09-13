69eaa20199785d03ffc3fe7f2e09ca9f81403043
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-09-13 08:20:51 +0530

    Time: 15 ms (77.05%), Space: 27.1 MB (49.51%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,22 +1,38 @@
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int cnt =  0;
+//         for(int i=0; i<nums.size(); i++){
+//             for(int j=0; j<nums.size(); j++){
+//                 if(nums[j] == nums[i]) cnt++;
+//             }
+//             if(cnt > nums.size()/2){
+//                 return nums[i];
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+// Tc: o(n2)
+// Sc: O(n)
+
+
+
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        int cnt = 0;
-        int maj = NULL;
+        map<int, int>m;
+        for(int i=0; i<nums.size(); i++){
+            m[nums[i]]++;
+        }
 
-        for(int i=0; i<n; i++){
-            if(cnt == 0){
-                cnt = 1;
-                maj = nums[i];
-            }
-            else if(nums[i] == maj){
-                cnt++;
-            }
-            else {
-                cnt--;
+        for(auto x : m){
+            if(x.second > nums.size()/2){
+                return x.first;
             }
         }
-        return maj;
+        return -1;
     }
 };
