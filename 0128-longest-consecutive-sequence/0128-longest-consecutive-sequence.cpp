50c28cdfce2c4ef099f44bc2a01f126fe66ec81a
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-09-15 16:10:34 +0530

    Time: 118 ms (62.6%), Space: 74.7 MB (33.97%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,30 +1,30 @@
-#include <vector>
-#include <algorithm>
-#include <limits.h>
-
 class Solution {
 public:
     int longestConsecutive(std::vector<int>& nums) {
-        if (nums.empty()) return 0; 
+    if (nums.empty()) return 0;
 
-        std::sort(nums.begin(), nums.end());
+    int longest = 1;
+    unordered_set<int> st;
+    //put all the array elements into set:
+    for (int i = 0; i < nums.size(); i++) {
+        st.insert(nums[i]);
+    }
 
-        int lastSmaller = INT_MIN;
-        int cnt = 1;
-        int longest = 0;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
+    //Find the longest sequence:
+    for (auto it : st) {
+        //if 'it' is a starting number:
+        if (st.find(it - 1) == st.end()) {
+            //find consecutive numbers:
+            int cnt = 1;
+            int x = it;
+            while (st.find(x + 1) != st.end()) {
+                x = x + 1;
+                cnt = cnt + 1;
             }
-            if (i > 0 && nums[i] == nums[i - 1] + 1) {
-                cnt += 1;
-            } else {
-                cnt = 1;
-            }
-            longest = std::max(longest, cnt);
+            longest = max(longest, cnt);
         }
-
-        return longest; 
+    }
+    return longest;
+ 
     }
 };
