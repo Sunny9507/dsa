a5445aa2e3acd8be007289d76afdd2064fef6a0e
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-09-16 23:47:15 +0530

    Time: 13 ms (20.8%), Space: 17 MB (26.97%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,28 +4,28 @@ public:
         int n = matrix.size(); // Number of rows
         int m = matrix[0].size(); // Number of columns
         
-        vector<int> row(n, 0); // Row array initialized to 0
-        vector<int> col(m, 0); // Column array initialized to 0
+        vector<int>x, y; 
 
-        // Traverse the matrix:
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (matrix[i][j] == 0) {
-                    // Mark ith index of row with 1:
-                    row[i] = 1;
-                    // Mark jth index of col with 1:
-                    col[j] = 1;
+                    x.push_back(i);
+                    y.push_back(j);
                 }
             }
         }
-
-        // Finally, mark all (i, j) as 0
-        // if row[i] or col[j] is marked with 1.
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (row[i] || col[j]) {
-                    matrix[i][j] = 0;
-                }
+        // row-> 0
+        for (int i = 0; i < x.size(); i++) {
+            int indx = x[i];
+            for(int j=0; j<m; j++){
+                matrix[indx][j] = 0;
+            }
+        }
+        // col-> 0
+        for (int i = 0; i < y.size(); i++) {
+            int indx = y[i];
+            for(int j=0; j<n; j++){
+                matrix[j][indx] = 0;
             }
         }
     }
