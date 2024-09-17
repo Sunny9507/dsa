c9f8e5c57a5bab39dc68176d0f21c3097b1305e6
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-09-17 23:32:50 +0530

    Time: 2831 ms (5.09%), Space: 33.2 MB (77.61%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        unordered_map<int, int> ump;  // To store the cumulative sum frequencies
-        int sum = 0, count = 0;
-        
-        // Initialize with sum 0 occurring once to handle cases where a subarray itself equals k
-        ump[0] = 1;
-        
-        for(int i = 0; i < n; i++) {
-            sum += nums[i];
-            int val = sum - k;
-            
-            // Check if (sum - k) exists in the map, indicating a valid subarray
-            if(ump.find(val) != ump.end()) {
-                count += ump[val];
-            }
-            
-            // Add the current cumulative sum to the map
-            ump[sum]++;
+        int cnt = 0;
+     for(int i=0; i<nums.size(); i++){
+         int sum = 0;
+        for(int j=i; j<nums.size(); j++){
+            sum = sum + nums[j];
+            if(sum == k){
+            cnt++;
         }
-        
-        return count;
+        }
+    } 
+     return cnt;
     }
 };
