2b691d1139eaf3956e10e38f28f0cae6708d4c8e
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-09-19 01:00:38 +0530

    Time: 12 ms (29.67%), Space: 18.8 MB (54.77%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        int x = n / 3;
-        
-        unordered_map<int, int> freq;
-        
-        // Count the frequency of each element
-        for (int num : nums) {
-            freq[num]++;
-        }
-        
-        vector<int> result;
-        
-        // Find elements with frequency greater than n/3
-        for (auto [num, count] : freq) {
-            if (count > x) {
-                result.push_back(num);
+        vector<int> ans;
+        vector<int> v;
+        for(int i = 0; i < nums.size(); i++) {
+            if(ans.empty() || find(ans.begin(), ans.end(), nums[i]) == ans.end()) {
+                int cnt = 0;
+                for(int j = 0; j < nums.size(); j++) {
+                    if(nums[j] == nums[i]) {
+                        cnt++;
+                    }
+                }
+                if(cnt > nums.size() / 3) {
+                    ans.push_back(nums[i]);
+                }
             }
+            if(ans.size() == 2) break;
         }
-        
-        return result;
+        return ans;
     }
 };
