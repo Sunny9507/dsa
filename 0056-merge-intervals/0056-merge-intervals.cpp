654d341978e165e5b06ae0cd8aa530bb6d403a3b
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-09-27 22:14:13 +0530

    Time: 27 ms (33.34%), Space: 23 MB (73.02%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,10 +6,15 @@ public:
         vector<vector<int>> ans;
 
         for(int i=0; i<intervals.size(); i++){
+
+// [[1,3],[2,6],[8,10],[15,18]] back [1][6]
+        // 8 > 6
+//  push [8][10]
             if(ans.empty() || intervals[i][0] > ans.back()[1]) {
                 ans.push_back(intervals[i]);
             }
             else{
+                //  [1,3],[2,6] replace 3 with 6
                 ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }        
         }
