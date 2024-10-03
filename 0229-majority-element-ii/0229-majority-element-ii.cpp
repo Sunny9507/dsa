00622f2d3ea60cb3eb2eb0961269e481a77cfd3a
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-10-03 19:17:48 +0530

    Time: 11 ms (42.42%), Space: 18.6 MB (84.59%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,30 +1,47 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n = nums.size(); //size of the array
-    vector<int> ls; // list of answers
+        int n = nums.size();
+        
+        int maj1    = NULL;
+        int count1 = 0;
 
-    for (int i = 0; i < n; i++) {
-        //selected element is v[i]:
-        // Checking if v[i] is not already
-        // a part of the answer:
-        if (ls.size() == 0 || ls[0] != nums[i]) {
-            int cnt = 0;
-            for (int j = 0; j < n; j++) {
-                // counting the frequency of v[i]
-                if (nums[j] == nums[i]) {
-                    cnt++;
-                }
+        int maj2    = NULL;
+        int count2 = 0;
+        int freq = floor(n/3);
+        
+        for(int i = 0; i<n; i++) {
+            if(nums[i] == maj1)
+                count1++;
+            else if(nums[i] == maj2)
+                count2++;
+            else if(count1 == 0) {
+                maj1 = nums[i];
+                count1 = 1;
+            } else if(count2 == 0) {
+                maj2 = nums[i];
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
             }
-
-            // check if frquency is greater than n/3:
-            if (cnt > (n / 3))
-                ls.push_back(nums[i]);
         }
+        
+        vector<int> result; //because atmost two elements can occur more than ⌊ n/3 ⌋ in an array of length n
+        count1 = 0;
+        count2 = 0;
+        for(int num:nums) {
+            if(num == maj1)
+                count1++;
+            else if(num == maj2)
+                count2++;
+        }
+        if(count1 > floor(n/3))
+            result.push_back(maj1);
+        if(count2 > floor(n/3))
+            result.push_back(maj2);
+        return result;
 
-        if (ls.size() == 2) break;
-    }
 
-    return ls;
     }
 };
