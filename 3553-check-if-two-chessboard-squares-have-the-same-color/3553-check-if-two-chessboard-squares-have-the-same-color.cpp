8e066c2b511c00ad6787953eac2f6a90e018759e
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-10-05 22:31:53 +0530

    Time: 0 ms (100%), Space: 7.9 MB (39.53%) - LeetHub

--- 3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.cpp
+++ 3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
     bool checkTwoChessboards(string coordinate1, string coordinate2) {
-        // Convert column (letter) to a number
-        int col1 = coordinate1[0] - 'a' + 1;
-        int row1 = coordinate1[1] - '0';
-        
-        int col2 = coordinate2[0] - 'a' + 1;
-        int row2 = coordinate2[1] - '0';
-        
-        // A square is black if (column + row) % 2 == 0, white otherwise
-        bool color1 = (col1 + row1) % 2 == 0;
-        bool color2 = (col2 + row2) % 2 == 0;
-        
-        // Return true if both have the same color
-        return color1 == color2;
+         int one = coordinate1[0] + coordinate1[1];
+        int two = coordinate2[0] + coordinate2[1];
+
+        return one % 2 == two % 2;
     }
 };
