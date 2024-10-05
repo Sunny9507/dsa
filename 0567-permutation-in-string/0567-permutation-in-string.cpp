c6bf5c78a4d2c9bff80207791ca80d45f0516ff8
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-10-05 21:51:59 +0530

    Time: 1051 ms (7.7%), Space: 60.5 MB (12.05%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int n = s1.size();
-        int m = s2.size();
-        
-        
-        if (n > m) return false;
-
-        vector<int> s1Freq(26, 0), windowFreq(26, 0);
-
-        
-        for (int i = 0; i < n; ++i) {
-            s1Freq[s1[i] - 'a']++;
-            windowFreq[s2[i] - 'a']++;
+        if(s1.length() > s2.length()){
+            return false;
         }
 
-        
-        for (int i = n; i < m; ++i) {
-            if (s1Freq == windowFreq) {
-                return true;  
+        sort(s1.begin(), s1.end());
+
+        for(int i=0; i<=s2.length()- s1.length(); i++){
+            string substring = s2.substr(i, s1.length());
+
+            sort(begin(substring), end(substring));
+            if(s1 == substring){
+                return true;
             }
-
-            windowFreq[s2[i - n] - 'a']--;  
-            windowFreq[s2[i] - 'a']++;   
         }
-
-        return s1Freq == windowFreq;
+        return false;
     }
 };
