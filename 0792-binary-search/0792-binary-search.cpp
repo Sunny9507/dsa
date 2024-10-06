0916d1ca34b09ce4c21955ea4cc068df213f9193
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-10-06 23:53:52 +0530

    Time: 30 ms (30.05%), Space: 30.1 MB (73.31%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
+    int binarySearch(vector<int>& nums, int l, int h, int target){
+        if(l > h) return -1;
+        
+        
+        int m = (l+h)/2;
+
+        if(nums[m] == target) return m;
+
+        else if(nums[m] < target) 
+            return binarySearch(nums, m + 1, h, target);
+
+            return binarySearch(nums, l, m-1, target);
+            
+        
+    }
+
+
     int search(vector<int>& nums, int target) {
-        int l=0, h=nums.size()-1;
-
-        while(l <= h){
-            int m = (l+h)/2;
-
-            if(nums[m] == target) return m;
-
-            else if(nums[m] < target) l=m+1;
-
-            else h = m-1;
-        }
-        return -1;
+        return binarySearch(nums, 0, nums.size()-1, target);
     }
 };
