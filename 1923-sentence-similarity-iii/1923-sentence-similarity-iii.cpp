da543391de3318125b54bea5acdff9e1bf40d66b
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-10-06 22:38:47 +0530

    Time: 4 ms (40.09%), Space: 8.5 MB (47.93%) - LeetHub

--- 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
+++ 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
     bool areSentencesSimilar(string s1, string s2) {
-        if(s1.length() < s2.length()){
+        if(s1.length() < s2.length()) {
             swap(s1, s2);
         }
 
-        vector<string> v1, v2;
-
+        deque<string> deq1, deq2;
         stringstream ss1(s1);
         string token;
-        while(ss1 >> token){
-            v1.push_back(token);
+        while(ss1 >> token) {
+            deq1.push_back(token);
         }
 
         stringstream ss2(s2);
-        while(ss2 >> token){
-            v2.push_back(token);
+        while(ss2 >> token) {
+            deq2.push_back(token);
         }
 
-        int i=0, j=v1.size()-1; // s1 (v1)
-        int k=0, l=v2.size()-1; // s2(v1)
-
-        while(k < v2.size() && i < v1.size() && v2[k] == v1[i]){
-            k++;
-            i++;
+        while(!deq1.empty() && !deq2.empty() && deq1.front() == deq2.front()) {
+            deq1.pop_front();
+            deq2.pop_front();
         }
 
-        while(l >= k && v2[l] == v1[j]){
-            j--;
-            l--;
+        while(!deq1.empty() && !deq2.empty() && deq1.back() == deq2.back()) {
+            deq1.pop_back();
+            deq2.pop_back();
         }
-
-        return l < k;
         
+        return deq2.empty();
+
     }
 };
