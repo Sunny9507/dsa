e2a3a5933681e03d3ad70153d5b8228f430d23d7
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-10-28 20:50:55 +0530

    Time: 174 ms (11.11%), Space: 111.5 MB (37.62%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
@@ -1,30 +1,16 @@
-//Approach-2 (Using set)
-//T.C : O(n) ///Note : O(n) because while loop will run max 5 times. So if constraint is given, then saying O(n) time complexity is fine.
-//S.C : O(n)
 class Solution {
 public:
     int longestSquareStreak(vector<int>& nums) {
-        unordered_set<int> st(begin(nums), end(nums));
-
-        int maxStreak = 0;
-
-        for(int &num : nums) {
-            int streak = 0;
-            long long curr = num;
-
-            while(st.find(curr) != st.end()) {
-                streak++;
-
-                if(curr*curr > 1e5) {
-                    break;
-                }
-
-                curr = curr*curr; //square
-            }
-
-            maxStreak = max(maxStreak, streak);
+        map<int, int>mp;
+        sort(nums.begin(), nums.end());
+        int res = -1;
+        for(int num: nums) {
+            int _sqrt = sqrt(num);
+            if(_sqrt*_sqrt == num && mp.find(_sqrt)!=mp.end()) {
+                mp[num] = mp[_sqrt]+1;
+                res = max(res, mp[num]);
+            } else mp[num] = 1;
         }
-
-        return maxStreak < 2 ? -1 : maxStreak;
+        return res;
     }
 };
