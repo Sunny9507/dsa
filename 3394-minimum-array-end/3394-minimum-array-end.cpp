5a00690aaf7be003f02367a2cbe1046584057ec1
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-11-09 09:47:40 +0530

    Time: 0 ms (100%), Space: 8.4 MB (82.17%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.cpp
+++ 3394-minimum-array-end/3394-minimum-array-end.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     long long minEnd(int n, int x) {
-        std::bitset<64> X(x), N(n - 1), result(0);
+        long long result = x, mask;
+        --n;  // Reducing n by 1 to exclude x from the iteration
 
-        int j = 0;
-        for (int i = 0; i < 64; i++) {
-            if (X[i]) {
-                result[i] = 1;
-            } else {
-                result[i] = N[j];
-                j++;
+        // Step 1: Iterate over each bit position with mask starting at 1 and
+        // shifting left
+        for (mask = 1; n > 0; mask <<= 1) {
+            // Step 2: If the corresponding bit in x is 0
+            if ((mask & x) == 0) {
+                // Set the bit in result based on the least significant bit of n
+                result |= (n & 1) * mask;
+                // Shift n to the right by 1 to process the next bit
+                n >>= 1;
             }
         }
 
-        return result.to_ullong();
+        return result;
     }
 };
