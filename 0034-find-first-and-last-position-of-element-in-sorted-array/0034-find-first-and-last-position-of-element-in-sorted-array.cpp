3fa3a55fd37e3abc6edecea0bc4f1ca4375eb9f5
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-12-01 13:38:24 +0530

    Time: 0 ms (100%), Space: 17.7 MB (5.73%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,36 +1,45 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> result = {-1, -1};
-        int left = binarySearch(nums, target, true);
-        int right = binarySearch(nums, target, false);
-        result[0] = left;
-        result[1] = right;
-        return result;        
-    }
-
-    int binarySearch(vector<int>& nums, int target, bool isSearchingLeft) {
-        int left = 0;
-        int right = nums.size() - 1;
-        int idx = -1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
+    int find_first_position(vector<int>& nums, int target) {
+        int l = 0, r = nums.size()-1;
+        int result = -1;
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            if(nums[mid] == target) {
+                result = mid; //possibly my answer
+                r = mid-1;     //but lets look at left more
+            } else if(nums[mid] > target) {
+                r = mid-1;
             } else {
-                idx = mid;
-                if (isSearchingLeft) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+                l = mid+1;
             }
         }
         
-        return idx;
-    }    
+        return result;
+    }
+    
+    int find_last_position(vector<int>& nums, int target) {
+        int l = 0, r = nums.size()-1;
+        int result = -1;
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            if(nums[mid] == target) {
+                result = mid; //possibly my answer
+                l = mid+1;   //but lets look at right more
+            } else if(nums[mid] > target) {
+                r = mid-1;
+            } else {
+                l = mid+1;
+            }
+        }
+        
+        return result;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int l = find_first_position(nums, target);
+        int r = find_last_position(nums, target);
+        
+        return {l, r};
+    }
 };
