b1010f1c2fcb91bcd793573bc27fffd2ad3bf141
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2024-12-05 00:04:29 +0530

    Time: 0 ms (100%), Space: 26.2 MB (6.47%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& arr) {
-        int n = arr.size();
-       if(n == 1) return arr[0];
-       if(arr[0] != arr[1]) return arr[0];
-       if(arr[n-1] != arr[n-2]) return arr[n-1];
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
 
-        int l = 1, h = n-2;
+        if(n == 1) return nums[0];
 
-        // we are in left
+        if(nums[0] != nums[1]) return nums[0];
+
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+
+        int l = 1, h = nums.size()-2;
+
         while(l <= h){
-            int m = (l+h)/2;
+            int m = l + (h - l) /2;
 
-            if(arr[m] != arr[m-1] && arr[m] != arr[m+1]){ return arr[m];}
+            if(nums[m-1] != nums[m] && nums[m] != nums[m+1]) return nums[m];
 
-            if((m % 2 == 1 && arr[m] == arr[m-1]) || m % 2 == 0 && arr[m] == arr[m+1]){
-                l = m+1;
+            if((m % 2 == 1 && nums[m] == nums[m-1]) ||
+             (m % 2 == 0 && nums[m] == nums[m+1])){
+                l = m + 1;
             }
-            // we are in right
             else{
                 h = m - 1;
             }
-        }
+        } 
         return -1;
     }
 };
