02b68f10ceb89deeabfcc96b7b1d9965ad31c957
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2025-01-19 13:48:59 +0530

    Time: 0 ms (100%), Space: 10.3 MB (91.02%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
-    bool isValid(vector<int>& nums, int k, int maxAllow) {
-        int subarrays = 1, currentSum = 0;
+    bool isValid(vector<int>& nums, int k, int maxAllow){
+        int first = 1, spplit = 0;
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (currentSum + nums[i] <= maxAllow) {
-                currentSum += nums[i];
-            } else {
-                subarrays++;
-                currentSum = nums[i];
-                if (subarrays > k) return false;
+        for(int i=0; i<nums.size(); i++){
+           
+            if(spplit + nums[i] <= maxAllow){
+                spplit += nums[i];
+            }
+            else{
+                first++;
+                spplit  = nums[i];
             }
         }
-        return true;
+        return first <= k;
     }
 
     int splitArray(vector<int>& nums, int k) {
-        int l = *max_element(nums.begin(), nums.end());
-        int h = accumulate(nums.begin(), nums.end(), 0);
+        int l = *max_element(nums.begin(), nums.end()),
+            h = accumulate(nums.begin(), nums.end(), 0);
 
-        while (l <= h) {
-            int mid = (l + h) / 2;
-
-            if (isValid(nums, k, mid)) {
+        int ans = -1;
+        while(l <= h){
+            int mid = (l + h)/2;
+             
+            if(isValid(nums, k, mid)){
+                ans = mid;
                 h = mid - 1;
-            } else {
+            }
+            else{
                 l = mid + 1;
             }
         }
-
-        return l;
+        return ans;
     }
 };
+
+
+
