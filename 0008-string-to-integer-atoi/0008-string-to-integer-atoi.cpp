c78601e2669697c0c0b70776df81eb3a687d41cb
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2025-01-28 22:41:08 +0530

    Time: 0 ms (100%), Space: 9.2 MB (26.04%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,33 +1,32 @@
 class Solution {
-public:
-    int myAtoi(string s) 
-    {
+    public :
+    int myAtoi(string s) {
+        if(s.length()==0) return 0;
+
         int i=0;
-        int sign=1;
-        long ans=0;
-        while(i<s.length() && s[i]==' ')
-            i++;
-        if(s[i]=='-')
-        {
-            sign=-1;
+         while(i<s.size() && s[i]== ' ') {
+             i++;
+         }     
+        s = s.substr(i); //i ---> last of string
+        
+        int sign = +1;
+        long ans = 0;
+        
+        if(s[0] == '-') sign = -1;
+        
+        int MAX = INT_MAX, MIN = INT_MIN;
+        i = (s[0] == '+' || s[0] == '-') ? 1 : 0;
+		
+        while(i < s.length()) {
+            if(s[0] == ' ' || !isdigit(s[i])) break;
+            
+            ans = ans * 10 + s[i]-'0';
+            if(sign == -1 && -1*ans < MIN) return MIN;
+            if(sign == 1 && ans > MAX) return MAX;
+            
             i++;
         }
-        else if(s[i]=='+')
-            i++;
-        while(i<s.length())
-        {
-            if(s[i]>='0' && s[i]<='9')
-            {
-                ans=ans*10+(s[i]-'0');
-                if(ans>INT_MAX && sign==-1)
-                    return INT_MIN;
-                else if(ans>INT_MAX && sign==1)
-                    return INT_MAX;
-                i++;
-            }
-            else
-                return ans*sign;
-        }
-        return (ans*sign);
+        
+        return (int)(sign*ans);
     }
 };
