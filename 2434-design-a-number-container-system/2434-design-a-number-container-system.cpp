22b6bfe2cc7a2a05e5c86c11986f133d37728637
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2025-02-08 23:14:54 +0530

    Time: 78 ms (95.94%), Space: 185.6 MB (93.13%) - LeetHub

---
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
@@ -0,0 +1,43 @@
+class NumberContainers {
+public:
+    //Space : O(n)
+    unordered_map<int, int> idxToNum;
+    unordered_map<int, priority_queue<int, vector<int>, greater<int>>> numToIdx;
+
+    NumberContainers() {
+        //do nothing    
+    }
+    
+    void change(int index, int number) {
+        idxToNum[index] = number; //O(1)
+
+        numToIdx[number].push(index); //O(log(n))
+    }
+    
+    int find(int number) {
+        if(!numToIdx.count(number)) {
+            return -1;
+        }
+
+        auto& pq = numToIdx[number];
+
+        while(!pq.empty()) { //O(k * logn)
+            int idx = pq.top(); //O(1)
+
+            if(idxToNum[idx] == number) {
+                return idx;
+            }
+
+            pq.pop(); //O(log(n))
+        }
+
+        return -1;
+    }
+};
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
