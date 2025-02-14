7faf29015b2d8f1732d63c9149de7a1ea454704b
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2025-02-14 23:35:16 +0530

    Time: 12 ms (15.14%), Space: 134.2 MB (33.19%) - LeetHub

--- 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.cpp
@@ -11,22 +11,25 @@
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        vector<int>v;
+        stack<int>st;
 
-        ListNode* temp = head;
-        while(temp){
-            v.push_back(temp->val);
-            temp = temp->next;
+        ListNode* curr = head;
+
+        while(curr){
+            st.push(curr->val);
+            curr = curr->next;
         }
 
-        int i=0, j = v.size()-1;
-        int result = 0;
-
-        while(i < j){
-            result = max(result, v[i]+v[j]); 
-            i++;
-            j--;
+        int n = st.size();
+        int cnt = 1, result = 0;
+        curr = head;
+        while(cnt <= n/2){
+            result = max(result, curr->val + st.top());
+            curr = curr->next;
+            st.pop();
+            cnt++;
         }
+        
         return result;
     }
 };
