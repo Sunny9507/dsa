1bf277217fcd2845f4c0cd676a912c688d245bf5
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2025-02-15 15:43:28 +0530

    Time: 6 ms (22.35%), Space: 79.5 MB (20.31%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -1,49 +1,45 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         stack<int> s1, s2;
 
-        while(l1){
+        // Push all values of l1 into stack s1
+        while (l1) {
             s1.push(l1->val);
             l1 = l1->next;
         }
-        while(l2){
+
+        // Push all values of l2 into stack s2
+        while (l2) {
             s2.push(l2->val);
             l2 = l2->next;
         }
 
-        int sum = 0, carry = 0;
-        ListNode* ans = new ListNode();
+        int carry = 0;
+        ListNode* ans = NULL;  // Start with NULL instead of an empty node
 
-        while(!s1.empty() || !s2.empty()){
-            if(!s1.empty()){
+        // Process both stacks until empty
+        while (!s1.empty() || !s2.empty() || carry) {
+            int sum = carry;
+
+            if (!s1.empty()) {
                 sum += s1.top();
                 s1.pop();
             }
 
-            if(!s2.empty()){
+            if (!s2.empty()) {
                 sum += s2.top();
                 s2.pop();
             }
-            ans->val = sum%10;
-            carry = sum/10;
 
-            ListNode* newNode = new ListNode(carry);
+            carry = sum / 10;
+
+            // Create a new node and add it to the front
+            ListNode* newNode = new ListNode(sum % 10);
             newNode->next = ans;
             ans = newNode;
-            sum = carry;
         }
 
-        return carry == 0 ? ans->next : ans; 
+        return ans; // No need for additional carry checks
     }
 };
