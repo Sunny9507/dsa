8c138590c29e6659ef8747a31daf14eb0a14be3c
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2025-02-17 00:07:10 +0530

    Time: 0 ms (100%), Space: 13.7 MB (17.75%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
@@ -1,43 +1,32 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     vector<ListNode*> splitListToParts(ListNode* head, int k) {
         ListNode* curr = head;
-        int L = 0;
-        while(curr) {
-            L++;
+        int length = 0;
+
+        while(curr){
+            length++;
             curr = curr->next;
         }
+
+        int eachBucket = length/k;
+        int extra = length % k;
         
-        int eachBucketNodes = L/k; //0
-        int remainderNodes  = L%k; //3
-        
-        vector<ListNode*> result(k, NULL);
-        curr           = head;
+        vector<ListNode* > result(k, NULL); // k nodes hone chahiye or sabhi me null daldo
+
+        curr = head;
         ListNode* prev = NULL;
-        
-        for(int i = 0; curr && i < k ; i++) {
-            
+
+        for(int i=0; i<k; i++){ 
             result[i] = curr;
-            
-            for(int count = 1; count <= eachBucketNodes + (remainderNodes > 0 ? 1 : 0); count++) {
+            for(int cnt=1; cnt <= eachBucket + (extra > 0 ? 1 : 0); cnt++){
                 prev = curr;
                 curr = curr->next;
             }
-            
-            prev->next = NULL;
-            remainderNodes--;
+            if(prev)
+                prev->next = NULL;
+            extra--;
         }
-        
         return result;
     }
 };
