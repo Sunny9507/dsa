a3676edc5fafa3bb4a979899e72694a57d3da603
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2025-02-19 23:55:16 +0530

    Time: 8 ms (27.56%), Space: 126.8 MB (34.08%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        if(!head || !head->next) {
+    ListNode* curr;
+
+    bool solve(ListNode* head) {
+        if(!head)
             return true;
-        }
-
-        ListNode* slow = head;
-        ListNode* fast = head;
-        ListNode* prev = NULL;
-
-
-        while(fast && fast->next) {
-            fast = fast->next->next;
-
-            ListNode* temp = slow->next;
-            slow->next = prev;
-            prev = slow;
-            slow = temp;
-        }
-
-        // If the number of nodes is odd, move slow to the next node
-        if (fast)
-            slow = slow->next;
         
-        while(prev && slow) {
-            if(prev->val != slow->val) {
-                return false;
-            }
-
-            prev = prev->next;
-            slow = slow->next;
+        if(!solve(head->next) || head->val != curr->val) {
+            return false;
         }
 
+        curr = curr->next;
         return true;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        curr = head;
+
+        return solve(head);
     }
 };
