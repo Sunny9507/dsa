ade28d1295dca42f930ff9b46578c120a2f4d115
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2025-02-20 21:34:35 +0530

    Time: 7 ms (72.62%), Space: 168.8 MB (58.52%) - LeetHub

--- 2573-remove-nodes-from-linked-list/2573-remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/2573-remove-nodes-from-linked-list.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
     ListNode* removeNodes(ListNode* head) {
-        stack<ListNode*>st;
-
-        ListNode* current = head;
-
-        while(current){
-            st.push(current);
-            current = current->next;
+        if (head == nullptr) {
+            return nullptr;
         }
+        ListNode* node = head;
+        ListNode* nxtGreater = removeNodes(node->next);
 
-        current = st.top();
-        st.pop();
-        int maximum = current->val;
-        ListNode* resultHead = new ListNode(maximum);
-
-        while(!st.empty()){
-            current = st.top();
-            st.pop();
-            if(current->val < maximum){
-                continue;
-            }
-            else {
-                ListNode* newNode = new ListNode(current->val);
-                newNode->next = resultHead;
-                resultHead = newNode;
-                maximum = current->val;
-            }
+        node->next = nxtGreater;
+        if (nxtGreater == nullptr || node->val >= nxtGreater->val) {
+            return node;
         }
-        return resultHead;
+        return nxtGreater;
     }
 };
