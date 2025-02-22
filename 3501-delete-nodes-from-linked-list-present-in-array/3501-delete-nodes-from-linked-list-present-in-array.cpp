851186036fb64524054582be1199312051b5e7dc
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2025-02-22 13:44:17 +0530

    Time: 88 ms (81.48%), Space: 263.5 MB (47.31%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
@@ -1,26 +1,13 @@
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        unordered_set<int> st(begin(nums), end(nums));
-        ListNode* prev = NULL;
-        ListNode* curr = head;
-
-
-        while (curr != NULL && st.count(curr->val)) {
-            head = curr->next;
-            curr = head;
-        }
-
-        while (curr != NULL) {
-            int currVal = curr->val;
-            if (!st.count(currVal)) {
-                prev = curr;
-                curr = curr->next;
-            } else {
-                prev->next = curr->next;
-                curr = curr->next;
-            }
-        }
-        return head;
-    }
+    ListNode tmp(0, head);
+    unordered_set<int> s(begin(nums), end(nums));
+    for (auto *p = &tmp; p->next != nullptr; )
+        if (s.contains(p->next->val))
+            p->next = p->next->next;
+        else
+            p = p->next;
+    return tmp.next;
+}
 };
