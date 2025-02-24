d5d879268f55c0018801185890ae2d92b0cc8e91
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2025-02-24 23:57:06 +0530

    Time: 14 ms (71.72%), Space: 58.3 MB (60.66%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        ListNode* temp = head;
-        vector<int>v;
-
-        while(temp){
-            v.push_back(temp->val);
-            temp = temp->next;
-        }   
-
-        sort(begin(v), end(v));
-        temp = head;
-        int i=0;
-        while(temp && i < v.size()){
-            temp->val = v[i];
-            i++;
-            temp = temp->next;
+        if (head == nullptr || head->next == nullptr) return head;
+        
+        ListNode* slow = head;
+        ListNode* fast = head->next->next; // Correct initialization
+        
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        return head;
+        
+        ListNode* l2 = sortList(slow->next);
+        slow->next = nullptr; // Break the list
+        
+        return merge(sortList(head), l2); // Correct recursive call
+    }
+    
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        if (l1 == nullptr) return l2;
+        if (l2 == nullptr) return l1;
+        
+        if (l1->val < l2->val) {
+            l1->next = merge(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = merge(l1, l2->next);
+            return l2;
+        }
     }
 };
