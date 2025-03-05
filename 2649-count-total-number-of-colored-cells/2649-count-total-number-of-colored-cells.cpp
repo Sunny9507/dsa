5bbecef13f83b2a28d98bb159e436df217463096
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2025-03-05 21:07:37 +0530

    Time: 0 ms (100%), Space: 8 MB (7.1%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        int time = 1;
-        long long cells = 1;
-
-        while(time <= n){
-            cells += (4 * (time-1));
-            time++;
-        }
-        return cells;
+        return 1 + (2* (long long) (n-1)* n);
     }
 };
