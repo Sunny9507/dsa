35a6319570051a15074c5207f3d2e5a442472cac
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2025-03-13 18:40:14 +0530

    Time: 6 ms (4.82%), Space: 9.2 MB (25.93%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,32 +1,32 @@
 class Solution {
-    public :
-    int myAtoi(string s) {
-        if(s.length()==0) return 0;
+public:
+    int convert(string &s, int index, long num, int sign) {
+        // Base case: Stop when non-digit is found or out of bounds
+        if (index >= s.length() || !isdigit(s[index])) 
+            return sign * num;  
 
-        int i=0;
-         while(i<s.size() && s[i]== ' ') {
-             i++;
-         }     
-        s = s.substr(i); //i ---> last of string
-        
-        int sign = +1;
-        long ans = 0;
-        
-        if(s[0] == '-') sign = -1;
-        
-        int MAX = INT_MAX, MIN = INT_MIN;
-        i = (s[0] == '+' || s[0] == '-') ? 1 : 0;
-		
-        while(i < s.length()) {
-            if(s[0] == ' ' || !isdigit(s[i])) break;
-            
-            ans = ans * 10 + s[i]-'0';
-            if(sign == -1 && -1*ans < MIN) return MIN;
-            if(sign == 1 && ans > MAX) return MAX;
-            
+        num = num * 10 + (s[index] - '0');  // Accumulate the number
+
+        // Clamp before further recursion
+        if (sign == 1 && num >= INT_MAX) return INT_MAX;
+        if (sign == -1 && -num <= INT_MIN) return INT_MIN;
+
+        return convert(s, index + 1, num, sign);  // Recurse to next digit
+    }
+
+    int myAtoi(string s) {
+        int i = 0, n = s.length(), sign = 1;
+
+        // Skip leading spaces
+        while (i < n && s[i] == ' ') i++;
+
+        // Handle sign
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-') sign = -1;
             i++;
         }
-        
-        return (int)(sign*ans);
+
+        return convert(s, i, 0, sign);
     }
 };
+
