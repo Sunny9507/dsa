630cd18c4de2342b041d4c630b13fcff8af3aad0
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2025-04-15 00:21:50 +0530

    Time: 4 ms (90.51%), Space: 11.1 MB (11.18%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.cpp
+++ 1656-count-good-triplets/1656-count-good-triplets.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
         int n = arr.size();
-        int cnt = 0; // Initialize the counter for good triplets
+        int cnt = 0;
         
         for(int i = 0; i < n - 2; i++) {
             for(int j = i + 1; j < n - 1; j++) {
