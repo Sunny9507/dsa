04e626960735632c5113ca26da6f5aaa82cc7c44
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2025-05-04 21:32:10 +0530

    Time: 0 ms (100%), Space: 26.1 MB (92.25%) - LeetHub

---
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dom) {
+        map<pair<int, int>, int> mp;
+
+            int cnt = 0;
+            for(auto& d : dom){
+                if(d[0] > d[1]) swap(d[0], d[1]);
+                cnt += mp[make_pair(d[0], d[1])]++;
+            }
+            return cnt;
+    }
+};
