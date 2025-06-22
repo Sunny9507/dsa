299d6b5e8835d01669903501b6d9e92647df04ad
Sunny9507 <163460105+Sunny9507@users.noreply.github.com>
2025-06-22 23:20:43 +0530

    Time: 3 ms (18.53%), Space: 11.2 MB (30.7%) - LeetHub

---
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int n = tickets.size();
+
+        queue<int> q;
+        for(int i=0; i<n; i++) q.push(i);
+
+        int time = 0;
+
+        while(tickets[k] != 0){
+            tickets[q.front()]--;
+            // Kya line me lagega
+            if(tickets[q.front()]){
+                q.push(q.front());
+                q.pop();
+            }else{
+                q.pop();
+            }
+
+                time++;
+        }
+        return time;
+    }
+};
